70e728221abe370eef3d59cf5c8b5409c3ceb5a4
sakethbikkina07 <sakethbikkina@gmail.com>
2026-07-12 14:46:29 +0530

    Refactor notification button in admin dashboard
    Updated button structure for notification icon.

--- Frontend/src/Component/Admin/adminDashboard.jsx
+++ Frontend/src/Component/Admin/adminDashboard.jsx
@@ -38,9 +38,8 @@ function AdminDashboard() {
         </div>
 
         <div className="flex items-center gap-4">
-          <button className="w-10 h-10 rounded-full border-1 border-gray-300 flex items-center justify-center bg-white shadow-sm cursor-pointer hover:border-2 hover:border-[#CBA358] hover:shadow-md transition">
-            <FiBell className="w-5 h-5 text-gray-600" />
-            <span className="absolute top-1 right-1 w-2 h-2 bg-yellow-600 rounded-full"></span>
+          <button className="relative w-10 h-10 rounded-full border-1 border-gray-300 flex items-center justify-center bg-white shadow-sm cursor-pointer hover:border-2 hover:border-[#CBA358] hover:shadow-md transition">
+          <FiBell className="w-5 h-5 text-gray-600" />
           </button>
           <div className="w-10 h-10 rounded-full border-1 border-gray-300 flex items-center justify-center bg-white shadow-sm cursor-pointer hover:border-2 hover:border-[#CBA358] hover:shadow-md transition">
             <FiUser className="w-5 h-5 text-gray-700" />
