f4e82ffb3d13e28b2e12b31e2a695d2118e3fd51
naga412 <nagalaxmijada5095@gmail.com>
2026-07-31 22:05:44 +0530

    B-house.js updated

--- Backend/models/house.js
+++ Backend/models/house.js
@@ -1,60 +1,60 @@
 import mongoose from "mongoose";
 
 const houseSchema = new mongoose.Schema({
-    ownerId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Owner",
-        required: true
-    },
-    houseName:{
-        type:String,
-        required:true
-    },
-    propertyType:{
-        type:String,
-        required:true
-    },
-    location:{
-        type:String,
-        required:true
-    },
-    price:{
-        type:String,
-        required:true
-    },
-    description:{
-        type:String,
-        default:""
-    },
-    rating:{
-        type:String,
-        default:""
-    },
-    bedrooms:{
-        type:Number,
-        default:""
-    },
-    bathrooms:{
-        type:Number,
-        default:""
-    },
-    areaSqft:{
-        type:Number,
-        default:""
-    },
-    features:{
-        type:String,
-        default:""
-    },
-    status:{
-        type:String,
-        enum:["available","sold","pending"],
-        default:"available"
-    },
-    image:{
-        type:String,
-        default:""
-    }
+  ownerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Owner",
+    required: true
+  },
+  houseName: {
+    type: String,
+    required: true
+  },
+  propertyType: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    default: ""
+  },
+  rating: {
+    type: String,
+    default: "0"
+  },
+  bedrooms: {
+    type: Number,
+    default: 0 
+  },
+  bathrooms: {
+    type: Number,
+    default: 0 
+  },
+  areaSqft: {
+    type: Number,
+    default: 0 
+  },
+  features: {
+    type: String,
+    default: ""
+  },
+  status: {
+    type: String,
+    enum: ["available", "sold", "pending"],
+    default: "available"
+  },
+  image: {
+    type: String,
+    default: ""
+  }
 });
 
 export default mongoose.model("House", houseSchema);
