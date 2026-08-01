f44da1b6e78f0743319c6a5ac7c14fba6a6246dd
sakethbikkina07 <sakethbikkina@gmail.com>
2026-08-01 13:47:53 +0530

    add fetch method

--- Frontend/src/Component/Admin/manageUsers.jsx
+++ Frontend/src/Component/Admin/manageUsers.jsx
@@ -24,8 +24,8 @@ function ManageUsers() {
   }, [])  
 
   const users = [
-    {id: 1, name: 'John Doe', email: 'john@example.com'},
-    {id: 2, name: 'Jane Smith', email: 'jane@example.com'}  
+    {id: 1, name: 'John Doe', joined: '2023-01-01', email: 'john@example.com'},
+    {id: 2, name: 'Jane Smith', joined: '2023-02-01', email: 'jane@example.com'}  
   ]
 
   return (
