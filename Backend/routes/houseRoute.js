9393456166e9d55e70990850e7287e1ac75ab97d
naga412 <nagalaxmijada5095@gmail.com>
2026-08-02 14:03:09 +0530

    b-routes updated

--- Backend/routes/houseRoute.js
+++ Backend/routes/houseRoute.js
@@ -1,15 +1,13 @@
 import express from "express";
-import { createHouseController, getAllHousesController, getHouseByIdController, updateHouseController, deleteHouseController, getAvailableHousesController, getHousesByOwnerController } from "../controllers/houseController.js";
+import { createHouseController,houseDetailsController,houseOwnerController, getAllHousesController, getHouseByIdController, updateDetailsController, deleteHouseController, getAvailableHousesController} from "../controllers/houseController.js";
 
 const router = express.Router();
 
-router.post("/", createHouseController);
+router.post("/", createHouseController,houseDetailsController,houseOwnerController);
 router.get("/", getAllHousesController);
 router.get("/status/:status", getAvailableHousesController);
 router.get("/:id", getHouseByIdController);
-router.put("/:id", updateHouseController);
+router.put("/:id", updateDetailsController);
 router.delete("/:id", deleteHouseController);
-// router.get("/filter/:houseName", filterHousesController);
-router.get("/owner/:ownerId", getHousesByOwnerController);
 
 export default router;
