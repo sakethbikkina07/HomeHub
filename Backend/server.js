e74e1658b6d8df2bae722e4ce5f66d5ee94b0ee4
sakethbikkina07 <sakethbikkina@gmail.com>
2026-08-05 13:28:12 +0530

    Changed port to 5001

--- Backend/server.js
+++ Backend/server.js
@@ -31,7 +31,7 @@ app.use("/api/wishlist", wishlistRoutes);
 
 // app.use("/api/count",countRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
